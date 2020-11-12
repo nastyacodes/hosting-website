47ca592639f14422a2dc0ca731d364b058404611
Anastasia Shpit <imshpit@gmail.com>
2020-11-13 00:09:22 +0200

    Modal opening and closing fixed

--- shared.js
+++ shared.js
@@ -1,10 +1,21 @@
 const backdrop = document.querySelector('.backdrop');
-const modal = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 
-for (let i = 0; i < selectPlanButtons; i++) {
+for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     });
+}
+
+const modalButtonNo = document.querySelector('.modal__action--negative');
+
+backdrop.addEventListener('click', closeModal);
+
+modalButtonNo.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
 }
