ae651fe805a7801397be326b9a7bc840ecd50768
Anastasia Shpit <imshpit@gmail.com>
2020-11-14 01:53:08 +0200

    Adjusting mobile nav and modal work

--- shared.js
+++ shared.js
@@ -1,21 +1,41 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const modalButtonNo = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-const modalButtonNo = document.querySelector('.modal__action--negative');
+backdrop.addEventListener('click', () => {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
 
-backdrop.addEventListener('click', closeModal);
-
-modalButtonNo.addEventListener('click', closeModal);
+if (modal) {
+    modalButtonNo.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+
+    backdrop.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', () => {
+   // mobileNav.style.display = 'block';
+   // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
